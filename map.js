515183882e272ff1cdf74c88e2317fb41d67d793
electrostat <agbyaj@gmail.com>
2016-10-11 20:07:28 -0400

    Working Version
    -initial upload of working version of GeoFi map

---
+++ map.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGFya291cm1ldGhvZCIsImEiOiI5Y2JmOGJhMDYzZDgyODBhYzQ3OTFkZWE3NGFiMmUzYiJ9.kp_5LMwcR79TKOERpkilAQ';
+    
+    // Set bounds to DMV
+    var bounds = [
+        [-77.247255, 38.764495], // Southwest coordinates
+        [-76.851141, 39.032550]  // Northeast coordinates
+    ];
+
+    
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/parkourmethod/cim5hb9c600jza0m473wrw6y6',
+        center: [-77.043132, 38.902705],
+        zoom: 16,
+        minZoom: 4
+    });
+
+    map.dragRotate.disable();
+    
+    //zoom in from search to 18
+    
+    var defLoc = "DC";
+    
+    //initial button focus
+    document.getElementById("DC").focus();
+    
+    $('.DC').on('click', function(e) {
+        map.flyTo({
+            center: [-77.043132, 38.902705],
+            zoom: 16
+        });
+        
+        defLoc = "DC";
+    });
+    
+    $('.NYC').on('click', function() {
+        map.flyTo({
+            center: [-73.998742, 40.725301],
+            zoom: 16
+        });
+        
+        defLoc = "NYC";
+    });
+    
+    $('.SF').on('click', function() {
+        map.flyTo({
+            center: [-122.410829, 37.785506],
+            zoom: 16
+        });
+        
+        defLoc = "SF";
+    });
+    
+    $('.PA').on('click', function() {
+        map.flyTo({
+            center: [-122.155012, 37.447295],
+            zoom: 16
+        });
+        
+        defLoc = "PA";
+    });
+    
+    $('.SM').on('click', function() {
+        map.flyTo({
+            center: [-118.480961, 34.001161],
+            zoom: 16
+        });
+        
+        defLoc = "SM";
+    });
+    
+    $('.CHI').on('click', function() {
+        map.flyTo({
+            center: [-87.628595, 41.881745],
+            zoom: 16
+        });
+        
+        defLoc = "CHI";
+    });
+    
+    map.on("click", function(){
+       //keep focus on buttons
+        document.getElementById(defLoc).focus();
+    });
+    
+    map.on("dragend", function(){
+       //keep focus on buttons
+        document.getElementById(defLoc).focus();
+    });
+    
+    map.on("dragstart", function(){
+       //keep focus on buttons
+        document.getElementById(defLoc).focus();
+    });
+});
