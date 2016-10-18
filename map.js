585e5723cafee08a249e3a7943978b1a559b3694
electrostat <agbyaj@gmail.com>
2016-10-18 12:29:56 -0400

    Format Time
    - bring time inline with google maps
    - H and min as indicators

--- map.js
+++ map.js
@@ -519,7 +519,7 @@ $(document).ready(function() {
         var h = Math.floor(d / 3600);
         var m = Math.floor(d % 3600 / 60);
         var s = Math.floor(d % 3600 % 60);
-        return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s); 
+        return ((h > 0 ? h + " h " + (m < 10 ? "0" : "") : "") + m + " min"); 
     }
     
 });
